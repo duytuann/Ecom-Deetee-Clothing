96aaed9f21660436b7d0416e327ac8fbf90a917f
duytuann <ddt.tuando@gmail.com>
2021-12-17 18:23:24 +0700

    Update =))

--- src/App.js
+++ src/App.js
@@ -1,44 +1,25 @@
-import React from 'react';
-import { Route, Routes, useParams, Link } from 'react-router-dom'
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
-import './App.css';
+import './App.css'
 
 import HomePage from './pages/homepage/homepage.component'
 
 const HatsPage = () => (
   <div>
-    <h1>HATS PAGE</h1>
+    <h1>HATS PAGE </h1>
   </div>
 )
-
-const TopicsList = () => (
-  <div>
-    <Link to='/'>Home</Link>
-    <h1>Topics List</h1>
-  </div>
-)
-
-const TopicsDetail = (props) => {
-  let { slug } = useParams()
-  console.log(props.history)
-  return (
-    <div>
-      <h1>Topics Detail {slug}</h1>
-    </div>
-  )
-}
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/hats' element={<HatsPage />} />
-        <Route path='/topics' element={<TopicsList />} />
-        <Route path='/topics/:slug' element={<TopicsDetail />} />
-      </Routes>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   )
 }
 
-export default App;
+export default App
