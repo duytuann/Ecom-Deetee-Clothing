9ba82c1c912541d3dbf0d4fe959f2a4622140060
duytuann <ddt.tuando@gmail.com>
2021-12-18 18:47:34 +0700

    Done Sign In Componnet

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,18 +5,16 @@ import { ReactComponent as Logo } from '../../assets/logo.svg'
 
 import './header.styles.scss'
 
-const Header = () => {
-    return (
-        <div className="header">
-            <Link className='logo-container' to='/' >
-                <Logo className='logo' />
-            </Link>
-            <div className="options">
-                <Link className="option" to="shop">SHOP</Link>
-                <Link className="option" to="shop">CONTACT</Link>
-            </div>
+const Header = () => (
+    <div className="header">
+        <Link className='logo-container' to='/' >
+            <Logo className='logo' />
+        </Link>
+        <div className="options">
+            <Link className="option" to="shop">SHOP</Link>
+            <Link className="option" to="shop">CONTACT</Link>
+            <Link className="option" to="signin">SIGN IN</Link>
         </div>
-    )
-}
-
+    </div>
+)
 export default Header
