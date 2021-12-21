225e64b76e12c4b8776fcee063600386f82f0c6b
duytuann <ddt.tuando@gmail.com>
2021-12-21 17:31:32 +0700

    Add Firebase -> SignIn with Google =))

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDOImFemdO-QwZH8N_lEfEhsPIDbN05bOs",
+    authDomain: "deetee-clothing-ecom.firebaseapp.com",
+    projectId: "deetee-clothing-ecom",
+    storageBucket: "deetee-clothing-ecom.appspot.com",
+    messagingSenderId: "817089336680",
+    appId: "1:817089336680:web:d4ae6df449662289dbb2dd",
+    measurementId: "G-P86PENZNHY"
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
+export const firestore = getFirestore(app)
+
+const provider = new GoogleAuthProvider()
+provider.setCustomParameters({ prompt: 'select_account' })
+
+export const signInWithGoogle = () => signInWithPopup(auth, provider)
+
+
+
+
