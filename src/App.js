0ef81cd62d7ac3e1ec858ae537b76f4d848d9535
duytuann <ddt.tuando@gmail.com>
2022-01-04 22:35:22 +0700

    addItem and removeItem in cart

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth, createUserProfileDocument, onSnapshot } from "./firebase/firebase.utils";
-import { setCurrentUser } from './redux/user/user.actions';
+import {
+  auth,
+  createUserProfileDocument,
+  onSnapshot,
+} from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -27,10 +34,10 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data(),
             },
-          })
+          });
 
-          console.log(this.state)
-        })
+          console.log(this.state);
+        });
       } else {
         setCurrentUser(userAuth);
       }
@@ -48,12 +55,13 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
             render={() =>
               this.props.currentUser ? (
-                <Redirect to='/' />
+                <Redirect to="/" />
               ) : (
                 <SignInAndSignUpPage />
               )
@@ -61,16 +69,16 @@ class App extends React.Component {
           />
         </Switch>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
-})
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+});
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,17 +1,31 @@
-import { createSelector } from 'reselect';
+import { createSelector } from "reselect";
 
-const selectCart = state => state.cart;
+const selectCart = (state) => state.cart;
 
 // const selectUser = state => state.user;
 
 export const selectCartItems = createSelector(
-    [selectCart],
-    (cart) => cart.cartItems
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hidden
 );
 
 export const selectCartItemsCount = createSelector(
-    [selectCartItems],
-    cartItems => cartItems.reduce(
-        (sumQuantity, cartItem) => sumQuantity + cartItem.quantity, 0
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (sumQuantity, cartItem) => sumQuantity + cartItem.quantity,
+      0
     )
-)
+);
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (sumQuantity, cartItem) => sumQuantity + cartItem.price * cartItem.quantity,
+    0
+  )
+);
