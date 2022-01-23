7e0bcb16259334f9c86197fa3dcdba1d11fd0c98
Duy Tuan <ddt.tuando@gmail.com>
2022-01-23 16:01:43 +0700

    Redux 2 and update shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Route } from "react-router-dom";
 
-import SHOP_DATA from './shop.data.js'
+import CollectionsOverview from "../../components/collections-overview/collections-overview.components.jsx";
+import CollectionPage from "../collection/collection.component.jsx";
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx'
+const ShopPage = ({ match }) => {
+  console.log(match);
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state
-        return (<div>
-            {
-                collections.map(({ id, ...otherCollectionProps }) =>
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                )
-            }
-        </div>)
-    }
-}
-
-export default ShopPage
+export default ShopPage;
