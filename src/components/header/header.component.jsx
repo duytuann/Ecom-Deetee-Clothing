60832140b2a5cf260a1e80697c19e5cd45b55eb7
Duy Tuan <ddt.tuando@gmail.com>
2022-01-28 15:52:33 +0700

    Styled Component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
@@ -10,41 +9,39 @@ import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
 
-import { ReactComponent as Logo } from "../../assets/logo.svg";
-
-import "./header.styles.scss";
+import {
+  HeaderContainer,
+  LogoContainer,
+  LogoSVG,
+  OptionsContainer,
+  OptionLink,
+} from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link className="option" to="shop">
-        SHOP
-      </Link>
-      <Link className="option" to="shop">
-        CONTACT
-      </Link>
+  <HeaderContainer>
+    <LogoContainer to="/">
+      <LogoSVG></LogoSVG>
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to="shop">SHOP</OptionLink>
+      <OptionLink to="shop">CONTACT</OptionLink>
       {currentUser ? (
-        <div
-          className="option"
+        <OptionLink
+          as="div"
           onClick={async (e) => {
             e.preventDefault();
             await signOut(auth);
           }}
         >
           SIGN OUT
-        </div>
+        </OptionLink>
       ) : (
-        <Link className="option" to="/signin">
-          SIGN IN
-        </Link>
+        <OptionLink to="/signin">SIGN IN</OptionLink>
       )}
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {hidden ? null : <CartDropdown />}
-  </div>
+  </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
