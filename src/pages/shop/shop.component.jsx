970c216cc615236e794718a7b8a3305a77044187
Duy Tuan <ddt.tuando@gmail.com>
2022-02-08 19:38:31 +0700

    HOC Higher Order Components Pattern - Spinner

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,12 +4,20 @@ import { connect } from "react-redux";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.components.jsx";
 import CollectionPage from "../collection/collection.component.jsx";
+import WithSpinner from "../../components/with-spinner/with-spinner.component.jsx";
 import { updateCollections } from "../../redux/shop/shop.actions";
 import { db } from "../../firebase/firebase.utils";
 import { collection, onSnapshot } from "firebase/firestore";
 import { convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+  state = {
+    loading: true,
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -21,18 +29,28 @@ class ShopPage extends React.Component {
       async (snapshot) => {
         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
         updateCollections(collectionsMap);
+        this.setState({ loading: false });
       }
     );
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
