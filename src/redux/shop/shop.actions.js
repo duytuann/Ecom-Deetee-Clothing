adf9f9dc0f08602d84db1eda8b5efa78758b8b8d
Duy Tuan <ddt.tuando@gmail.com>
2022-02-11 14:46:36 +0700

    Container pattern

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,37 @@
 import ShopActionTypes from "./shop.types";
+import { getDocs, collection } from "firebase/firestore";
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
+import {
+  db,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+export const fetchCollectionsStart = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap,
 });
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return async (dispatch) => {
+    const collectionRef = collection(db, "collections");
+    dispatch(fetchCollectionsStart());
+
+    const collectionSnap = await getDocs(collectionRef);
+    const collectionsMap = convertCollectionsSnapshotToMap(collectionSnap);
+    dispatch(fetchCollectionsSuccess(collectionsMap));
+
+    // .catch((error) =>
+    //   dispatch(fetchCollectionsFailure(error.message))
+    // );
+  };
+};
+
